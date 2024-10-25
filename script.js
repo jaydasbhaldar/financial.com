b1aa85b00a08ed51c9d7a6844406262114d143c4
jaydasbhaldar <jbhaldar01@gmail.com>
2024-10-25 10:35:14 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById('contactForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Form submitted!');
+    // You can add your form submission logic here, e.g., send the data via AJAX.
+});
